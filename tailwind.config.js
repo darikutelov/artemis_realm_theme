010cc70ddf4b7758ea4b749bad308978166d65cf
darikutelov <dari@kutelov.com>
2024-06-19 18:46:45 +0300

    Product page

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,11 @@ module.exports = {
     "./snippets/**/*.liquid",
   ],
   theme: {
-    extend: {},
+    extend: {
+      height: {
+        94: "22rem",
+      },
+    },
   },
   plugins: [],
 };
